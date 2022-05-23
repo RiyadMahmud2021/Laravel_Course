ceefe7b1e8f29523a45c7af8a629302bc9c8da12
Riyad Mahmud <riyad.prof.bd@gmail.com>
2022-05-23 18:19:17 +0600

    changes

--- Frontend_Site/resources/views/Components/AllCourses.blade.php
+++ Frontend_Site/resources/views/Components/AllCourses.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-4 p-1 text-center">
             <div class="card">
                 <div class="text-center">
-                    <img style="width: 200px; height: 200px;" class="w-100" src="{{$key->course_img}}" alt="Card image cap">
+                    <img style="width: 200px; height: 200px;" class="w-100" src="{{ $key->course_img }}" alt="Card image cap">
                     <h5 class="service-card-title mt-4">{{$key->course_name}}</h5>
                     <h6 class="service-card-subTitle p-0 ">{{$key->course_des}}</h6>
                     <h6 class="service-card-subTitle p-0 ">Fee: {{$key->course_fee}}/-</h6>
